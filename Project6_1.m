a6c29acc27db570576133b5047d67cfa8cb71621
khart-3 <57810103+khart-3@users.noreply.github.com>
2021-04-16 23:21:08 -0400

    Add files via upload

--- Project6_1.m
+++ Project6_1.m
@@ -6,8 +6,8 @@ x3=[];
 x4=[];
 x5=[];
 x6=[];
-y_1= [];
-y_2=[];
+Y_1= [];
+Y_2=[];
 for i=1:10000
     x1(i)=(R2-R1)*rand+R1;
     x2(i)=(R2-R1)*rand+R1;
@@ -15,8 +15,8 @@ for i=1:10000
     x4(i)=(R2-R1)*rand+R1;
     x5(i)=(R2-R1)*rand+R1;
     x6(i)=(R2-R1)*rand+R1;
-    y_1(i)= x1(i)+x2(i)+x3(i)+x4(i)+x5(i)+x6(i);
-    y_2(i)=x1(i)+(x2(1)*.5)+(x3(i)*.8)+(x4(i)+1.8)+(x5(i)*.3)+(x6(i)*.5);
+    Y_1(i)= x1(i)+x2(i)+x3(i)+x4(i)+x5(i)+x6(i);
+    Y_2(i)=x1(i)+(x2(1)*.5)+(x3(i)*.8)+(x4(i)+1.8)+(x5(i)*.3)+(x6(i)*.5);
 end
 
 figure(1)
@@ -28,11 +28,26 @@ subplot(3,2,5);plot(x5);title('x5');
 subplot(3,2,6);plot(x6);title('x6');
 
 figure(2)
-subplot(2,1,1);hist(y_1,20);title('PDF - y1');%change 20 to 30 for better curve
-subplot(2,1,2);hist(y_2,20);title('PDF - y2');%change 20 to 30 for better curve
+subplot(2,1,1);hist(Y_1,30);title('PDF - Y1');
+subplot(2,1,2);hist(Y_2,30);title('PDF - Y2');
 
 figure(3)
-subplot(2,1,1);cdfplot(y_1);title('CDF - y1');
-subplot(2,1,2);cdfplot(y_2);title('CDF - y2');
+subplot(2,1,1);cdfplot(Y_1);title('CDF - Y1');
+subplot(2,1,2);cdfplot(Y_2);title('CDF - Y2');
+
+%% b
+
+sum1=0;
+for i=1:length(Y_1)
+    sum1=sum1+Y_1(i);
+end
+Mn=sum1/length(Y_1)
+
+sum2=0;
+for i=length(Y_1)
+    sum2=sum2+(Y_1(i)-Mn)^2;
+end
+sigma2=sum2/length(Y_1);
+
 
     
